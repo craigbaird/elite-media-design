73ce9b9c641ff43a4a4198cde00fcc61ea3ec8ac
Craig Baird <bairdcraig@gmail.com>
2020-05-12 21:43:53 -0500

    basic get to api data

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  apiData: any;
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://www.instagram.com/goblue42agency/?__a=1').subscribe(data => {
+      this.apiData = data;
+      console.log('Data From API ', this.apiData);
+    });
   }
 
 }
